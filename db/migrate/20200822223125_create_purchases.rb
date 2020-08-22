13c9b1f84acc3496e7cdaec7f0ed60a688ee99bc
Nicolas <nicolas@elsemillerocoworking.com>
2020-08-22 19:32:42 -0300

    added purchase models

---
+++ db/migrate/20200822223125_create_purchases.rb
@@ -0,0 +1,14 @@
+class CreatePurchases < ActiveRecord::Migration[6.0]
+  def change
+    create_table :purchases do |t|
+      t.string :material
+      t.string :provaider
+      t.integer :quantity
+      t.integer :price, default: 100
+      t.integer :date
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
