f03db4d1e5848e790c427b859917cc00c750e56f
Nicolas <nicolas@elsemillerocoworking.com>
2020-09-13 09:28:54 -0300

    seeds users + purchases

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Destroy all seeds"
+Purchase.destroy_all
+User.destroy_all
+
+puts "Starting seeding process"
+
+10.times do
+ puts "Creating Users"
+ user = User.create!(email: Faker::Internet.email, password: 123456789, first_name: Faker::Internet.name, last_name: Faker::FunnyName.name)
+ 2.times do
+  puts "Creating Purchase"
+  Purchase.create!(user: user, material: Faker::Beer.brand, provaider: Faker::Company.name, quantity: 100, price: 10, date: Faker::Date.between(from: '2019-09-23', to: '2020-09-25'))
+ end
+end
+
+puts "Finished seeding!"
+
