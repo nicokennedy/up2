33f3434afac620a861e9aab8abbb93d5b3f2c4bb
Nicolas <nicolas@elsemillerocoworking.com>
2020-10-07 23:40:50 -0300

    purchases 80%

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,28 @@
 class PurchasesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!
 
   def index
     @purchases = Purchase.all
   end
+
+  def show
+    @purchase = Purchase.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ puts "Starting seeding process"
 
 10.times do
  puts "Creating Users"
- user = User.create!(email: Faker::Internet.email, password: 123456789, first_name: Faker::Internet.name, last_name: Faker::FunnyName.name)
+ user = User.create!(email: Faker::Internet.email, password: 123456789, first_name: Faker::Name.name, last_name: Faker::FunnyName.name)
  2.times do
   puts "Creating Purchase"
   Purchase.create!(user: user, material: Faker::Beer.brand, provaider: Faker::Company.name, quantity: 100, price: 10, date: Faker::Date.between(from: '2019-09-23', to: '2020-09-25'))
